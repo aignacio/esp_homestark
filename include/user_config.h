7c76767c1475b57f8d1480941428233f64fddfd1
aignacio <anderson.igns@gmail.com>
2015-01-27 01:28:45 -0200

    Initial commit..

---
+++ include/user_config.h
@@ -0,0 +1,28 @@
+#ifndef _USER_CONFIG_H_
+#define _USER_CONFIG_H_
+
+#define CFG_HOLDER	0x00FF55A4	/* Change this value to load default configurations */
+#define CFG_LOCATION	0x3C	/* Please don't change or if you know what you doing */
+#define CLIENT_SSL_ENABLE
+
+/*DEFAULT CONFIGURATIONS*/
+
+#define MQTT_HOST			"192.168.0.12" //or "mqtt.yourdomain.com"
+#define MQTT_PORT			1883
+#define MQTT_BUF_SIZE		1024
+#define MQTT_KEEPALIVE		120	 /*second*/
+
+#define MQTT_CLIENT_ID		"Dev_Homestark"
+#define MQTT_USER			"DVES_USER"
+#define MQTT_PASS			"DVES_PASS"
+
+#define STA_SSID "Wi-fi Anderson"
+#define STA_PASS "wifi3102230"
+#define STA_TYPE AUTH_WPA2_PSK
+
+#define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
+
+#define DEFAULT_SECURITY	0
+#define QUEUE_BUFFER_SIZE		 		2048
+
+#endif
